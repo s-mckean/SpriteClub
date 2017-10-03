f853a828838e73b9d8375cd1f32df0fb17a63e11
Drew Thompson <d_thompson6@u.pacific.edu>
2017-10-03 14:34:40 -0700

    In theaters near you: Jumping - a romantic story of a ball and its space key

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
+	public float speed = 1.75f;
+	public float jumpForce = 250f;
 	Rigidbody rb;
+	bool canJump;
 
 	// Use this for initialization
 	void Start () 
 	{
 		rb = GetComponent<Rigidbody> ();
+		canJump = true;
 	}
 
 	void FixedUpdate()
@@ -21,10 +24,29 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce (movement * speed);
+
+		if (Input.GetKeyDown(KeyCode.Space) && canJump) {
+			rb.AddForce (Vector3.up * jumpForce);
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Ground")) {
+			canJump = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other) 
+	{
+		if (other.CompareTag("Ground")) {
+			canJump = false;
+		}
+	}
+
 }
