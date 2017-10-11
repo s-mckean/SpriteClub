31b197ee046145513d7b06f451566fa89b99d720
Drew Thompson <d_thompson6@u.pacific.edu>
2017-10-11 15:11:20 -0700

    Collecting coins is now more beautiful

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     // Moved speed and speedCap to GameManager since they need to be accessible to other scripts
-    public float jumpForce, maxDepth;
+    public float jumpForce, maxDepth, partySpeed, fadeTime;
 	public bool restrictingDepth;
 	public Vector3 startPosition;
+	public Color defaultColor, targetColor;
 
 	Rigidbody rb;
 	float radius, health;
 
-
 	Collider[] colliders;
+
+	Renderer renderer;
+	float timeLeft = 1.0f;
+	bool partying = false;
 
 	// Use this for initialization
 	void Start () 
@@ -22,6 +26,8 @@ public class PlayerController : MonoBehaviour {
         startPosition = transform.position;
 		rb = GetComponent<Rigidbody> ();
 		health = 1f;
+		renderer = GetComponent<Renderer> ();
+		renderer.sharedMaterial.color = defaultColor;
 	}
 
 	void FixedUpdate()
@@ -82,6 +88,36 @@ public class PlayerController : MonoBehaviour {
 	public void SetHealth(float newHealth)
 	{
 		health = newHealth;
+	}
+
+	public void PartyTime()
+	{
+		if (!partying) {
+			StartCoroutine ("LerpIt");
+		}
+	}
+
+	IEnumerator LerpIt()
+	{
+		partying = true;
+		while (timeLeft > Time.deltaTime) {
+			renderer.sharedMaterial.color = Color.Lerp (defaultColor, targetColor, Time.deltaTime * partySpeed / timeLeft);
+			timeLeft -= Time.deltaTime * partySpeed;
+			yield return null;
+		}
+
+		timeLeft = 1.0f;
+
+		float hangover = partySpeed / fadeTime;
+		while (timeLeft > Time.deltaTime) {
+			renderer.sharedMaterial.color = Color.Lerp (targetColor, defaultColor, Time.deltaTime * hangover  / timeLeft);
+			timeLeft -= Time.deltaTime * hangover;
+			yield return null;
+		}
+
+		renderer.sharedMaterial.color = defaultColor;
+		timeLeft = 1.0f;
+		partying = false;
 	}
 
 	void OnTouchDown(Point axisData) 
