c7784d324718285cd09d8ede010fc93d771cb327
Drew Thompson <d_thompson6@u.pacific.edu>
2017-10-14 11:43:30 -0700

    Added a self-destruct for blocks to clear up clutter. Change prefab script delay value to edit destruct time

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+
+	public float destroyTime = 1.5f;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (this.CompareTag ("Bottom")) { 
+			return;
+		}
+
+		if (other.CompareTag("Ground")) {
+			Destroy (this.gameObject, destroyTime);
+		}
+	}
+}
