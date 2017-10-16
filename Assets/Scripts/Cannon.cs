981f32221ca99160b7b4c1db2c0904faa46aa345
maximveligan <maximveligan@gmail.com>
2017-10-16 03:52:16 -0700

    Created a level

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -10,14 +10,15 @@ public class Cannon : MonoBehaviour {
 	private float xValue;
 	private GameObject wheel;
 	private float angle;
+	private float blastForce = 4500;
 	// Use this for initialization
 	void Start () {
 		wheel = GameObject.FindGameObjectWithTag ("Wheel");
 		player = GameObject.FindGameObjectWithTag ("Player");
 		angle = wheel.transform.rotation.eulerAngles.z;
-		yValue = (angle / 90) * 1000f;
+		yValue = (angle / 90) * blastForce;
 		Debug.Log (angle);
-		xValue = ((Mathf.Abs(angle - 90)) / 90) * 1000f;
+		xValue = ((Mathf.Abs(angle - 90)) / 90) * blastForce;
 		Debug.Log (wheel.transform.rotation.eulerAngles.z);
 	}
 
@@ -28,6 +29,7 @@ public class Cannon : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag == "Player") {
 			Debug.Log ("Gottem");
+			player.GetComponent<PlayerController> ().transform.position = wheel.transform.position;
 			player.GetComponent<PlayerController>().cannonBoost(new Vector3 (xValue, yValue, 0.0f));
 			Debug.Log (yValue);
 		}
